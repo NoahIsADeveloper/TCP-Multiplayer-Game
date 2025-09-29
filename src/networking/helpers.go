195b78b1425a7800ed698d7907f6d51031da0eb0
Noah <kalervo.noah@gmail.com>
2025-09-29 12:31:39 +1000

    more data types for reading/writing

--- src/networking/helpers.go
+++ src/networking/helpers.go
@@ -3,6 +3,7 @@ package networking
 import (
 	"fmt"
 	"net"
+	"encoding/binary"
 )
 
 const PACKET_MAX_SIZE = 1_000_000
@@ -13,6 +14,39 @@ const PACKET_MAX_SIZE = 1_000_000
 // Packet length: VarInt
 // Packet ID: VarInt
 // Data: any
+
+func readPacket(conn net.Conn) (int, []byte, error) {
+	length, err := decodeVarInt(conn)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	if length < 1 {
+		return 0, nil, fmt.Errorf("packet too short")
+	}
+
+	if length > PACKET_MAX_SIZE {
+		return 0, nil, fmt.Errorf("packet too large: %d", length)
+	}
+
+	packetData := make([]byte, length)
+	totalRead := 0
+	for totalRead < length {
+		read, err := conn.Read(packetData[totalRead:])
+		if err != nil {
+			return 0, nil, err
+		}
+		totalRead += read
+	}
+	
+	packetID, idBytes := decodeVarIntFromBytes(packetData)
+	if idBytes == 0 {
+		return 0, nil, fmt.Errorf("invalid packet ID")
+	}
+	
+	data := packetData[idBytes:]
+	return packetID, data, nil
+}
 
 func decodeVarInt(conn net.Conn) (int, error) {
 	var result int
@@ -55,6 +89,14 @@ func decodeVarIntFromBytes(data []byte) (int, int) {
 	return result, bytesRead
 }
 
+func sendPacket(conn net.Conn, packetID int, data []byte) error {
+	encodedID := encodeVarInt(packetID)
+	packet := append(encodedID, data...)
+	length := encodeVarInt(len(packet))
+	_, err := conn.Write(append(length, packet...))
+	return err
+}
+
 func encodeVarInt(value int) []byte {
 	var result []byte
 	for {
@@ -71,49 +113,66 @@ func encodeVarInt(value int) []byte {
 	return result
 }
 
-func readPacket(conn net.Conn) (int, []byte, error) {
-	length, err := decodeVarInt(conn)
-	if err != nil {
-		return 0, nil, err
-	}
-
-	if length < 1 {
-		return 0, nil, fmt.Errorf("packet too short")
-	}
-
-	if length > PACKET_MAX_SIZE {
-		return 0, nil, fmt.Errorf("packet too large: %d", length)
-	}
-
-	packetData := make([]byte, length)
-	totalRead := 0
-	for totalRead < length {
-		read, err := conn.Read(packetData[totalRead:])
-		if err != nil {
-			return 0, nil, err
-		}
-		totalRead += read
-	}
-	
-	packetID, idBytes := decodeVarIntFromBytes(packetData)
-	if idBytes == 0 {
-		return 0, nil, fmt.Errorf("invalid packet ID")
-	}
-	
-	data := packetData[idBytes:]
-	return packetID, data, nil
-}
-
-func sendPacket(conn net.Conn, packetID int, data []byte) error {
-	encodedID := encodeVarInt(packetID)
-	packet := append(encodedID, data...)
-	length := encodeVarInt(len(packet))
-	_, err := conn.Write(append(length, packet...))
-	return err
+func appendVarInt(list *[]byte, value int) {
+	*list = append(*list, encodeVarInt(value)...)
 }
 
 func appendString(list *[]byte, str string) {
-	data:= []byte(str)
-	*list = append(*list, encodeVarInt(len(data))...)
+	data := []byte(str)
+	appendVarInt(list, len(data))
 	*list = append(*list, data...)
+}
+
+func appendPosition(list *[]byte, x int16, y int16) {
+	buf := make([]byte, 4)
+	binary.BigEndian.PutUint16(buf[0:2], uint16(x))
+	binary.BigEndian.PutUint16(buf[2:4], uint16(y))
+	*list = append(*list, buf...)
+}
+
+func readVarInt(data []byte, offset *int) (int, error) {
+	var result int
+	var shift uint
+
+	for {
+		if *offset >= len(data) {
+			return 0, fmt.Errorf("varint: unexpected end of data")
+		}
+		b := data[*offset]
+		*offset++
+
+		result |= int(b & 0x7F) << shift
+
+		if b & 0x80 == 0 {
+			break
+		}
+		shift += 7
+		if shift > 35 {
+			return 0, fmt.Errorf("varint too big")
+		}
+	}
+	return result, nil
+}
+
+func readString(data []byte, offset *int) (string, error) {
+	length, err := readVarInt(data, offset)
+	if err != nil {
+		return "", err
+	}
+	if length < 0 || *offset+length > len(data) {
+		return "", fmt.Errorf("string: invalid length %d", length)
+	}
+	str := string(data[*offset : *offset+length])
+	*offset += length
+	return str, nil
+}
+
+func readPosition(data []byte, offset *int) (int16, int16, error) {
+	if *offset + 4 > len(data) {
+		return 0, 0, fmt.Errorf("position: not enough data")
+	}
+	x := int16(binary.BigEndian.Uint16(data[*offset : *offset+2]))
+	y := int16(binary.BigEndian.Uint16(data[*offset+2 : *offset+4]))
+	*offset += 4
+	return x, y, nil
 }
