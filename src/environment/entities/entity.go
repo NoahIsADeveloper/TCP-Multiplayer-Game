362ce5e0b09ff80cb8bb4451c5bcede82a6eb70a
Noah <kalervo.noah@gmail.com>
2025-09-30 09:24:36 +1000

    entity in range method

--- src/environment/entities/entity.go
+++ src/environment/entities/entity.go
@@ -16,6 +16,12 @@ func (entity *Entity) GetPosition() (uint16, uint16) {
 	return entity.X, entity.Y
 }
 
+func (entity *Entity) InRange(x, y, distance uint16) bool {
+	distX := int32(entity.X) - int32(x)
+	djstY := int32(entity.Y) - int32(y)
+	return (distX * distX + djstY * djstY) <= (int32(distance)*int32(distance))
+}
+
 func CreateEntity() *Entity {
 	entity := &Entity{kind: 0}
 	entity.Move(1 << 15, 1 << 15)
