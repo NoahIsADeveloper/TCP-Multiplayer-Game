da7d2862ddbb0f3e3f17d477f602fe617577f97e
Noah <kalervo.noah@gmail.com>
2025-10-04 15:16:00 +1000

    i hate default go formatting

--- src/environment/entities/entity.go
+++ src/environment/entities/entity.go
@@ -3,34 +3,31 @@ package entities
 import "sync"
 
 type Entity struct {
-	x        uint16
-	y        uint16
+	x uint16
+	y uint16
 	rotation float32
-	mutex    sync.RWMutex
+	mutex sync.RWMutex
 }
 
 func (entity *Entity) Update(deltaTime float64) {}
 
 func (entity *Entity) Move(x uint16, y uint16, rotation float32) {
-	entity.mutex.Lock()
-	defer entity.mutex.Unlock()
+	entity.mutex.Lock(); defer entity.mutex.Unlock()
 	entity.x = x
 	entity.y = y
 	entity.rotation = rotation
 }
 
 func (entity *Entity) GetPosition() (uint16, uint16, float32) {
-	entity.mutex.RLock()
-	defer entity.mutex.RUnlock()
+	entity.mutex.RLock(); defer entity.mutex.RUnlock()
 	return entity.x, entity.y, entity.rotation
 }
 
 func (entity *Entity) InRange(x, y, distance uint16) bool {
-	entity.mutex.RLock()
-	defer entity.mutex.RUnlock()
+	entity.mutex.RLock(); defer entity.mutex.RUnlock()
 	distX := int32(entity.x) - int32(x)
 	distY := int32(entity.y) - int32(y)
-	return (distX*distX + distY*distY) <= (int32(distance) * int32(distance))
+	return (distX * distX + distY * distY) <= (int32(distance) * int32(distance))
 }
 
 func NewEntity() *Entity {
